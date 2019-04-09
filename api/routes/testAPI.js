d1a2bdc3d5554dccc36963590fe78fcbf604a8eb
Isaias Labrador <ilabradors@eafit.edu.co>
2019-04-09 15:18:08 -0500

    Cambios al puerto de API => 3000 => 9000

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('ASI QUE ASI SE MANDAN LINEASdasdasAS DE TEXTO');
+    res.send('RESPUESTA DE EXPRESS');
 });
 
 module.exports = router;
