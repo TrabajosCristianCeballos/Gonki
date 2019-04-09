66d63d3c37cc477f5a8d23676c6851baa11bc216
ilabradors <33398937+ilabradors@users.noreply.github.com>
2019-04-09 10:18:12 -0500

    Add files via upload
    Esta es api sencilla a la que react le hace la peticion.

---
+++ api/routes/testAPI.js
@@ -0,0 +1,9 @@
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.send('ASI QUE ASI SE MANDAN LINEASdasdasAS DE TEXTO');
+});
+
+module.exports = router;
