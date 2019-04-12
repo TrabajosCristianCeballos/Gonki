9863b11d0a7853d305a286b1d202bdd8aeedaabc
samael <saturnoadv@gmail.com>
2019-04-11 21:26:43 -0500

    Added initial API

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,49 @@
 var express = require('express');
+var Evento = require('../schemasdbs/Eventos')
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+const findDocuments = function(db, callback) {
+
+
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    // Database Name
+    const dbName = 'admin';
+    const client = new MongoClient('mongodb://gonkicosmos:io3ChxxbY1AmKRtjmrW8LDVHF3zQF0b3Zs16c0CRdQsVHCNmVrFc9dTSJxFwfRwyIvOamqtrSJaftyImynRVLw==@gonkicosmos.documents.azure.com:10255/?ssl=true', { useNewUrlParser: true });
+
+
+    // Use connect method to connect to the Server
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+
+        console.log("is connected:::", client.isConnected())
+        const db = client.db(dbName);
+
+        var cursor = db.collection('Eventos').find();
+        // console.log(cursor)
+        console.log("WE HAVE A CURSOR:::::::::::")
+
+        // // Get the documents collection
+        const collection = db.collection('Eventos');
+
+        // // Find some documents
+        collection.find({}).toArray((err, docs) => {
+            assert.equal(err, null);
+            // assert.equal(err, null);
+            console.log("Found the following records");
+            console.log(docs)
+            res.send(docs);
+            client.close();
+        });
+    });
 });
+
+
 
 module.exports = router;
--- mongodb/env.js
+++
@@ -1,9 +0,0 @@
-
-const env ={
-  dbName:'gonkicosmos',
-  key:'io3ChxxbY1AmKRtjmrW8LDVHF3zQF0b3Zs16c0CRdQsVHCNmVrFc9dTSJxFwfRwylvOamqtrSJaftylmynRVLw==',
-  port:'10255'
-};
- module.export ={
-   env
- };
